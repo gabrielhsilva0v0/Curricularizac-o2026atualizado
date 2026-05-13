1ed187efe73047e474958ee5f34974fedb8e974e
gabrielhsilva0v0 <gabriel.hsilva@unifebe.edu.br>
2026-05-13 15:16:01 -0300

    Teste de trocas dde classe usando java, integração das perguntas e sua  funcionalidade de troca

--- Script.js
+++ Script.js
@@ -1,18 +1,34 @@
 const r3Opt = [1,2,1,3,2]
-const rVF = [0,1,1,0,1]
-
-document.getElementById("pergunta").innerHTML = "Pergunta";
+const rVF = [0,1,1,0,1,0,1]
+const listPerguntas = 
+[
+    "A fenarreco acontece uma vez por semana",
+    "No centro de Brusque tem um Zoológico",
+    "Brusque tem uma cultura alemã",
+    "O nome do rio de Brusque é Rio Dragão",
+    "Brusque existe a mais de 100 anos",
+    "O marreco é um tipo de cachorro",
+    "Um doce típico de Brusque é a Cuca"
+]
+var perg = 0;
+document.getElementById("pergunta").innerHTML = listPerguntas[perg];
 document.getElementById("VF").addEventListener("submit", function(event){
 
     event.preventDefault();
 
     let resposta = event.submitter.value;
-
-    if(resposta == rVF[0]){
-        document.getElementById("saida").innerHTML = "correto";
+    
+    if(resposta == rVF[perg]){
+        document.getElementById("saida").classList = "result";
+        document.getElementById("saida").innerHTML = perg;
+        if (perg < (rVF.length)-1){
+          perg++;
+          document.getElementById("pergunta").innerHTML = listPerguntas[perg];  
+        }
+        
     }
     else{
-        document.getElementById("saida").innerHTML = "incorreto";
+        document.getElementById("saida").className = "hidden"
     }
 
 });
