78ee24b4c2c1c2670f7fd1a1b45d6b4e26f70c1f
gabrielhsilva0v0 <gabriel.hsilva@unifebe.edu.br>
2026-05-15 15:21:06 -0300

    audio implementation

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-const r3Opt = [1, 2, 1, 3, 2]
+
 const rVF = [0, 1, 1, 0, 1, 0, 1]
 const listPerguntas =
     [
@@ -10,30 +10,41 @@ const listPerguntas =
         "O marreco é um tipo de cachorro",
         "Um doce típico de Brusque é a Cuca"
     ]
+    const audioList = 
+    [
+        "assets\\audiosVF\\fenarreco.mp3",
+        "assets\\audiosVF\\zoologico.mp3",
+        "assets\\audiosVF\\cultura.mp3",
+        "assets\\audiosVF\\rio.mp3",
+        "assets\\audiosVF\\idade.mp3",
+        "assets\\audiosVF\\marreco.mp3",
+        "assets\\audiosVF\\cuca.mp3"
+    ]
 var perg = 0;
 var hold = 0;
+var audioToPlay = document.getElementById("audio");
 document.getElementById("pergunta").innerHTML = listPerguntas[perg];
+document.getElementById("audioSrc").src = audioList[perg];
 document.getElementById("VF").addEventListener("submit", function (event) {
 
     event.preventDefault();
-
+    document.getElementById("audioSrc").src = audioList[perg];
+    audioToPlay.load();
+    
     let resposta = event.submitter.value;
     if (hold == 0) {
         if (resposta == rVF[perg]) {
             hold = 1;
-            document.getElementById("resultado").classList = "animB1";
-            document.getElementById("estrela").classList = "animS1";
             document.getElementById("animation").classList = "corretoAnim"
             setTimeout(() => {
                 document.getElementById("animation").className = ""
-                document.getElementById("resultado").classList = "hidden";
-                document.getElementById("estrela").classList = "hidden";
                 if (perg < (rVF.length) - 1) {
                     perg++;
                     document.getElementById("pergunta").innerHTML = listPerguntas[perg];
+                    audioToPlay.play();
                 }
                 hold = 0;
-            }, 2500);
+            }, 850);
 
 
         }
